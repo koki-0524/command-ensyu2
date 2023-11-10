717cb2acb41e0f6a8077ff6418fe988c4ee61ae2
yoshikawa koki <k.yoshikawa.sys23@morijyobi.ac.jp>
2023-11-10 09:46:23 +0900

    BMI

--- Main.java
+++ Main.java
@@ -2,20 +2,37 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
         Scanner scanner = new Scanner(System.in);
-        System.out.print("身長（メートル単位）を入力してください: ");
-        double height = scanner.nextDouble();
-        System.out.print("体重（キログラム単位）を入力してください: ");
-        double weight = scanner.nextDouble();
+
+        System.out.println("1人目の情報を入力してください:");
+        double height1 = getInput("身長（メートル単位）", scanner);
+        double weight1 = getInput("体重（キログラム単位）", scanner);
+
+        System.out.println("2人目の情報を入力してください:");
+        double height2 = getInput("身長（メートル単位）", scanner);
+        double weight2 = getInput("体重（キログラム単位）", scanner);
+
         scanner.close();
 
-        double bmi = weight / (height * height);
+        double bmi1 = calculateBMI(height1, weight1);
+        System.out.printf("1人目のBMIは %.2f です。\n", bmi1);
+        printBMIMessage(bmi1);
 
-        double health = 22.0 * (height * height);
+        double bmi2 = calculateBMI(height2, weight2);
+        System.out.printf("2人目のBMIは %.2f です。\n", bmi2);
+        printBMIMessage(bmi2);
+    }
 
-        System.out.printf("あなたのBMIは %.2f です。\n", bmi);
+    private static double getInput(String prompt, Scanner scanner) {
+        System.out.print(prompt + ": ");
+        return scanner.nextDouble();
+    }
 
+    private static double calculateBMI(double height, double weight) {
+        return weight / (height * height);
+    }
+
+    private static void printBMIMessage(double bmi) {
         if (bmi < 18.5) {
             System.out.println("あなたは低体重です。適正体重に近づけるためにバランスの取れた食事を摂りましょう。");
         } else if (bmi < 25.0) {
